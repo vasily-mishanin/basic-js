90247207a33e48f468269f6ec2f05a62d6f920b4
vasily-mishanin <vasilymishanin@gmail.com>
2022-05-02 11:36:12 +0300

    feat: add viginere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,25 +20,107 @@ const { NotImplementedError } = require("../extensions/index.js");
  *
  */
 class VigenereCipheringMachine {
-  // constructor(option) {
-  //   this.isDirectMachine =
-  //     option === undefined || option === true ? true : false;
-  // }
+  constructor(option) {
+    this.isDirectMachine =
+      option === undefined || option === true ? true : false;
+  }
+
+  getKeyString(message, key) {
+    return (
+      key.toUpperCase().repeat(Math.ceil(message.length / key.length)) + key
+    );
+  }
+
+  makeVigenereTable(givenAlphabet) {
+    const shiftAlphabet = (alphabetArr) =>
+      [alphabetArr.slice(1), ...alphabetArr.slice(0, 1)].join("");
+    let originalAlphabet = givenAlphabet;
+    let alphabet = originalAlphabet;
+    let vigTable = new Object();
+    for (let char of alphabet) {
+      if (char === "A") {
+        vigTable[char] = this.mapShiftedChars(originalAlphabet, alphabet);
+      } else {
+        alphabet = shiftAlphabet(alphabet);
+        vigTable[char] = this.mapShiftedChars(originalAlphabet, alphabet);
+      }
+    }
+    return vigTable;
+  }
+
+  mapShiftedChars(original, shifted) {
+    let charMap = {};
+    let i = 0;
+    original.split("").forEach((char) => {
+      charMap[char] = shifted[i];
+      i++;
+    });
+    return charMap;
+  }
 
   encrypt(message, key) {
-    throw new NotImplementedError("Not implemented");
+    // throw new NotImplementedError("Not implemented");
+    const ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const VIG_TABLE = this.makeVigenereTable(ALPHABET);
+
     if (!message || !key) {
       throw new Error("Incorrect arguments!");
     }
     message = message.toUpperCase();
+    let encryptedMessage = "";
+    let keyString = this.getKeyString(message, key);
+    let keyLetterIndex = 0;
+    let alphabetArr = ALPHABET.split("");
+    for (let i = 0; i < message.length; i++) {
+      let messageChar = message[i];
+      let keywordChar = keyString[keyLetterIndex];
+      if (alphabetArr.includes(messageChar)) {
+        encryptedMessage += VIG_TABLE[keywordChar][messageChar];
+        keyLetterIndex++;
+      } else {
+        encryptedMessage += messageChar;
+      }
+    }
+    encryptedMessage = this.isDirectMachine
+      ? encryptedMessage
+      : encryptedMessage.split("").reverse().join("");
+    return encryptedMessage;
   }
 
   decrypt(encryptedMessage, key) {
-    throw new NotImplementedError("Not implemented");
+    //throw new NotImplementedError("Not implemented");
+
+    const ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const VIG_TABLE = this.makeVigenereTable(ALPHABET);
+
     if (!encryptedMessage || !key) {
       throw new Error("Incorrect arguments!");
     }
     encryptedMessage = encryptedMessage.toUpperCase();
+    let decryptedMessage = "";
+    let keyString = this.getKeyString(encryptedMessage, key);
+    let keyLetterIndex = 0;
+    let alphabetArr = ALPHABET.split("");
+    for (let i = 0; i < encryptedMessage.length; i++) {
+      let encryptedMessageChar = encryptedMessage[i];
+      let keywordChar = keyString[keyLetterIndex];
+      if (alphabetArr.includes(encryptedMessageChar)) {
+        for (let [original, shifted] of Object.entries(
+          VIG_TABLE[keywordChar]
+        )) {
+          if (shifted === encryptedMessageChar) {
+            decryptedMessage += original;
+          }
+        }
+        keyLetterIndex++;
+      } else {
+        decryptedMessage += encryptedMessageChar;
+      }
+    }
+    decryptedMessage = this.isDirectMachine
+      ? decryptedMessage
+      : decryptedMessage.split("").reverse().join("");
+    return decryptedMessage;
   }
 }
 
